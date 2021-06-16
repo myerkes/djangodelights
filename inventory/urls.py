f87ac2dedca9a152465afb96035548d33f4ecd98
myerkes <mattyerkes23@gmail.com>
2021-06-16 14:19:00 -0400

    Create menu view

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemList.as_view(), name='menu')
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,17 @@
+from inventory.models import MenuItem
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def index(request):
     return render(request, 'inventory/index.html')
+
+class MenuItemList(ListView):
+    model = MenuItem
+    context_object_name = 'menuitem_list'
+    template_name = 'inventory/menuitem_list.html'
