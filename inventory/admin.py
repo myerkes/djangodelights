97c54ed438486ba03ab13def4fdd5bb8aa52d23c
myerkes <mattyerkes23@gmail.com>
2021-06-16 11:32:06 -0400

    Register models with admin

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Ingredient, MenuItem, RecipeRequirements, Purchase, Order
+
+admin.site.register(Ingredient)
+admin.site.register(MenuItem)
+admin.site.register(RecipeRequirements)
+admin.site.register(Purchase)
+admin.site.register(Order)
